d1a59dfbcf8d2bb79b704e0b35d4088572301d6c
Umang Metaliya <167159513+umapi0407@users.noreply.github.com>
2026-02-25 10:35:18 +0530

    Update CORS origin to include production URL

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:8080", // Vite frontend
+  origin: "http://localhost:8080" || "https://kryyvix-website.vercel.app", // Vite frontend
 
 }));
 
